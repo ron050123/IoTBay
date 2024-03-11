c6bb83ef84cd0ff14b454e078225b0110b9ffe1a
keegenwall-is <keegen.wallis@gmail.com>
2024-03-11 12:16:45 +1100

    Registration button on home page

---
+++ IoTBay.web/Controllers/RegistrationController.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics;
+using IoTBay.web.Data;
+using Microsoft.AspNetCore.Mvc;
+using IoTBay.web.Models;
+using IoTBay.web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IoTBay.web.Controllers;
+
+public class RegistrationController : Controller
+{
+    
+}
