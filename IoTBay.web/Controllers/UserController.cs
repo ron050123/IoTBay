cd5a10036661fd23b01f3a6d4cd8c2699ca1f7d4
Brandon <brandon.nguyen-4@student.uts.edu.au>
2024-03-20 22:18:02 +1100

    UserController file added for user input being put into the database

---
+++ IoTBay.web/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IoTBay.web.Models.Entities;
+using IoTBay.web.Data;
+
+namespace IoTBay.web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public UserController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("~/Views/Home/Register.cshtml");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(Usr user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return Content("User registered successfully!");
+            }
+            else
+            {
+                return View("~/Views/Home/Register.cshtml", user);
+            }
+        }
+    }
+}
