55e87fb702a3004f753bc0ec2fff4c12861975df
InsertOverusedUsername <Randy.wu100@gmail.com>
2024-03-23 21:59:20 +1100

    Create LoginController.cs

---
+++ IoTBay.web/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics;
+using IoTBay.web.Data;
+using Microsoft.AspNetCore.Mvc;
+using IoTBay.web.Models;
+using IoTBay.web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IoTBay.web.Controllers;
+
+public class LoginController : Controller {
+    
+}
